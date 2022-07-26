11b2c617498a841ac247e6fc67ba4daeb6a12a42
caneregin <36435160+caneregin@users.noreply.github.com>
2022-07-27 01:03:58 +0300

    Logic bitti

--- src/App.js
+++ src/App.js
@@ -5,91 +5,65 @@ import 'semantic-ui-css/semantic.min.css'
 import { useState } from 'react';
 
 function App() {
-  let prevArr = []
-  let nextArr = []
   const [calcvalue, setCalcvalue] = useState("")
   const [conclusion, setConclusion] = useState("")
-  const [prevvalue, setPrevvalue] = useState(prevArr)
-  const handleclick1 = () => {
-    setPrevvalue([...prevvalue, 1])
-  }
-  const handleclick2 = () => {
-    setPrevvalue([...prevvalue, 2])
-  }
-  const handleclick3 = () => {
-    setPrevvalue([...prevvalue, 3])
-  }
-  const handleclick4 = () => {
-    setPrevvalue([...prevvalue, 4])
-  }
-  const handleclick5 = () => {
-    setPrevvalue([...prevvalue, 5])
-  }
-  const handleclick6 = () => {
-    setPrevvalue([...prevvalue, 6])
-  }
-  const handleclick7 = () => {
-    setPrevvalue([...prevvalue, 7])
-  }
-  const handleclick8 = () => {
-    setPrevvalue([...prevvalue, 8])
-  }
-  const handleclick9 = () => {
-    setPrevvalue([...prevvalue, 9])
-  }
-  const handleclick0 = () => {
-    setPrevvalue([...prevvalue, 0])
-  }
+
   const handleclickpi = () => {
-    setPrevvalue([...prevvalue, 0])
+    setConclusion("3,1415926535897932384626433832795")
   }
   const handleclicke = () => {
-    setCalcvalue("2,7182818284590452353602874713527")
+    setConclusion("2,7182818284590452353602874713527")
   }
   const handleclicksquare = () => {
-    let tempsquare = prevvalue
+    let tempsquare = calcvalue
     let squareequal = tempsquare * tempsquare
-    setCalcvalue(squareequal)
+    setConclusion(squareequal)
   }
   const handleclickce = () => {
-    setPrevvalue([...prevvalue, 0])
+    setCalcvalue("")
+    setConclusion("")
   }
   const handleclickcube = () => {
-    setPrevvalue([...prevvalue, 0])
-  }
-  const handleclickdel = () => {
-    setPrevvalue([...prevvalue, 0])
+    let tempsquare = calcvalue
+    let squareequal = tempsquare * tempsquare * tempsquare
+    setConclusion(squareequal)
   }
   const handleclicksquareroot = () => {
-    setPrevvalue([...prevvalue, 0])
-  }
-  const handleclickdivide = () => {
-    setPrevvalue([...prevvalue, 0])
-  }
-  const handleclickmultiplication = () => {
-    setPrevvalue([...prevvalue, 0])
-  }
-  const handleclickminus = () => {
-    setPrevvalue([...prevvalue, 0])
-  }
-  const handleclickplus = () => {
-    setPrevvalue([...prevvalue, 0])
+    let tempsquare = calcvalue
+    let sqrt = Math.sqrt(tempsquare);
+    setConclusion(sqrt)
   }
   const handleclicklog = () => {
-    setPrevvalue([...prevvalue, 0])
-  }
-  const handleclickcomma = () => {
-    setPrevvalue([...prevvalue, 0])
+    let templog = calcvalue
+    let log = Math.log(templog);
+    setConclusion(log)
   }
   const handleclickequal = () => {
-    setPrevvalue([...prevvalue, 0])
+    setCalcvalue(conclusion)
   }
+  const operators = ["+", "-", ".", "/", "*"]
+
   const updateCalc = value => {
+    if (
+      operators.includes(value) && calcvalue === '' ||
+      operators.includes(value) && operators.includes(calcvalue.slice(-1)
+      )
+    ) { return; }
     setCalcvalue(calcvalue + value)
+    if (!operators.includes(value)) {
+      setConclusion(eval(calcvalue + value).toString())
+    }
+  }
+  const del = () => {
+    if (calcvalue === "") {
+      return
+    }
+    const value = calcvalue.slice(0, -1)
+    setCalcvalue(value)
   }
   return (
     <div className="App">
-      <h2>Hesap Makinesi</h2>
+      <h2>Hesap Makinesi{conclusion}</h2>
       <h2>
         {conclusion ? <span>(0)</span> : ""}
         {calcvalue || "0"}
@@ -105,7 +79,7 @@ function App() {
           <Grid.Column onClick={handleclickce}>
             CE
           </Grid.Column>
-          <Grid.Column onClick={handleclickdel}>
+          <Grid.Column className='operators' onClick={del}>
             DEL
           </Grid.Column>
         </Grid.Row>
@@ -120,7 +94,7 @@ function App() {
           <Grid.Column onClick={handleclicksquareroot}>
             KAREKÖK
           </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("/")}>
+          <Grid.Column className='operators' onClick={() => updateCalc("/")}>
             BÖL
           </Grid.Column>
         </Grid.Row>
@@ -134,12 +108,12 @@ function App() {
           <Grid.Column onClick={() => updateCalc("9".toString())}>
             9
           </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("*")}>
+          <Grid.Column className='operators' onClick={() => updateCalc("*")}>
             ÇARP
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-        <Grid.Column onClick={() => updateCalc("4".toString())}>
+          <Grid.Column onClick={() => updateCalc("4".toString())}>
             4
           </Grid.Column>
           <Grid.Column onClick={() => updateCalc("5".toString())}>
@@ -148,12 +122,12 @@ function App() {
           <Grid.Column onClick={() => updateCalc("6".toString())}>
             6
           </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("-")}>
+          <Grid.Column className='operators' onClick={() => updateCalc("-")}>
             ÇIKAR
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-        <Grid.Column onClick={() => updateCalc("1".toString())}>
+          <Grid.Column onClick={() => updateCalc("1".toString())}>
             1
           </Grid.Column>
           <Grid.Column onClick={() => updateCalc("2".toString())}>
@@ -162,7 +136,7 @@ function App() {
           <Grid.Column onClick={() => updateCalc("3".toString())}>
             3
           </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("+")}>
+          <Grid.Column className='operators' onClick={() => updateCalc("+")}>
             TOPLA
           </Grid.Column>
         </Grid.Row>
@@ -173,10 +147,10 @@ function App() {
           <Grid.Column onClick={() => updateCalc("0".toString())}>
             0
           </Grid.Column>
-          <Grid.Column onClick={handleclickcomma}>
+          <Grid.Column onClick={() => updateCalc(".")}>
             ,
           </Grid.Column>
-          <Grid.Column onClick={handleclickequal}>
+          <Grid.Column className='operators' onClick={handleclickequal}>
             EŞİTTİR
           </Grid.Column>
         </Grid.Row>

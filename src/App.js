0df0be3c9af7fc9c77b3151a44ee91c08e98c460
caneregin <36435160+caneregin@users.noreply.github.com>
2022-07-27 11:08:30 +0300

    Semantic ui düzenlendi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Grid, Image } from 'semantic-ui-react'
+import { Table, Grid, Label } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import { useState } from 'react';
 
@@ -63,98 +63,54 @@ function App() {
   }
   return (
     <div className="App">
-      <h2>Hesap Makinesi{conclusion}</h2>
+      <h2>Hesap Makinesi</h2>
       <h2>
-        {conclusion ? <span>(0)</span> : ""}
+        {conclusion ? <span className='concspan'>({conclusion})</span> : <span className='concspan'>(0)</span>}
         {calcvalue || "0"}
       </h2>
-      <Grid columns={4} divided>
-        <Grid.Row>
-          <Grid.Column onClick={handleclickpi}>
-            pi
-          </Grid.Column>
-          <Grid.Column onClick={handleclicke}>
-            e
-          </Grid.Column>
-          <Grid.Column onClick={handleclickce}>
-            CE
-          </Grid.Column>
-          <Grid.Column className='operators' onClick={del}>
-            DEL
-          </Grid.Column>
-        </Grid.Row>
+      <Table style={{border:"none",textAlign:"center"}}>
+      <Table.Body>
+          <Table.Row >
+            <Table.Cell className='operators' onClick={handleclickpi}>pi</Table.Cell>
+            <Table.Cell className='operators' onClick={handleclicke}>e</Table.Cell>
+            <Table.Cell className='operators' onClick={handleclickce}>CE</Table.Cell>
+            <Table.Cell className='operators' onClick={del}>DEL</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell className='operators' onClick={handleclicksquare}>x²</Table.Cell>
+            <Table.Cell className='operators' onClick={handleclickcube}>x³</Table.Cell>
+            <Table.Cell className='operators' onClick={handleclicksquareroot}>√</Table.Cell>
+            <Table.Cell className='operators' onClick={() => updateCalc("/")}>÷</Table.Cell>
+          </Table.Row>
+        
 
-        <Grid.Row>
-          <Grid.Column onClick={handleclicksquare}>
-            KARE
-          </Grid.Column>
-          <Grid.Column onClick={handleclickcube}>
-            KÜP
-          </Grid.Column>
-          <Grid.Column onClick={handleclicksquareroot}>
-            KAREKÖK
-          </Grid.Column>
-          <Grid.Column className='operators' onClick={() => updateCalc("/")}>
-            BÖL
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column onClick={() => updateCalc("7".toString())}>
-            7
-          </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("8".toString())}>
-            8
-          </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("9".toString())}>
-            9
-          </Grid.Column>
-          <Grid.Column className='operators' onClick={() => updateCalc("*")}>
-            ÇARP
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column onClick={() => updateCalc("4".toString())}>
-            4
-          </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("5".toString())}>
-            5
-          </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("6".toString())}>
-            6
-          </Grid.Column>
-          <Grid.Column className='operators' onClick={() => updateCalc("-")}>
-            ÇIKAR
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column onClick={() => updateCalc("1".toString())}>
-            1
-          </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("2".toString())}>
-            2
-          </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("3".toString())}>
-            3
-          </Grid.Column>
-          <Grid.Column className='operators' onClick={() => updateCalc("+")}>
-            TOPLA
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column onClick={handleclicklog}>
-            LOG
-          </Grid.Column>
-          <Grid.Column onClick={() => updateCalc("0".toString())}>
-            0
-          </Grid.Column>
-          <Grid.Column onClick={() => updateCalc(".")}>
-            ,
-          </Grid.Column>
-          <Grid.Column className='operators' onClick={handleclickequal}>
-            EŞİTTİR
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+        
+          <Table.Row>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("7".toString())}>7</Table.Cell>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("8".toString())}>8</Table.Cell>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("9".toString())}>9</Table.Cell>
+            <Table.Cell className='operators' onClick={() => updateCalc("*")}>*</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("4".toString())}>4</Table.Cell>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("5".toString())}>5</Table.Cell>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("6".toString())}>6</Table.Cell>
+            <Table.Cell className='operators' onClick={() => updateCalc("-")}>-</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("1".toString())}>1</Table.Cell>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("2".toString())}>2</Table.Cell>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("3".toString())}>3</Table.Cell>
+            <Table.Cell className='operators' onClick={() => updateCalc("+")}>+</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell className='operatorsother' onClick={handleclicklog}>LOG</Table.Cell>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc("0".toString())}>0</Table.Cell>
+            <Table.Cell className='operatorsother' onClick={() => updateCalc(".")}>,</Table.Cell>
+            <Table.Cell className='operators' onClick={handleclickequal}>=</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
     </div>
   );
 }
